a5096b2c13c307c392825fdc169b54ba9c673bce
Anthony Parker-Perry <anthony.parker-perry@datasym.co.uk>
2023-12-01 13:30:24 +0000

    Dy1 pt1

---
+++ AdventOfCode/Day1/Program.cs
@@ -0,0 +1,18 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+var lines = File.ReadLines(@"Data.txt");
+var sumOfCalibration = 0;
+
+foreach (string line in lines)
+{
+    var numbers = string.Concat(line.Where(char.IsDigit));
+    var firstNum = numbers[0];
+    var lastNum = numbers[numbers.Length - 1];
+    var twoDigits = $"{firstNum}{lastNum}";
+    int.TryParse(twoDigits, out int bigNum);
+    sumOfCalibration += bigNum;
+}
+
+Console.WriteLine($"result = {sumOfCalibration}");
+Console.Read();
