c9fb75133f90bca5696545649d7b843e3b758ac8
Anthony Parker-Perry <anthony.parker-perry@datasym.co.uk>
2023-12-04 16:53:47 +0000

    Day4

---
+++ AdventOfCode/Day4/Program.cs
@@ -0,0 +1,15 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using Day4;
+
+Console.WriteLine("Hello, World!");
+
+var lines = File.ReadLines(@"Data.txt");
+
+var engine = new ScratchCardEngine(lines);
+
+var res = engine.ProcessValues();
+
+Console.WriteLine(res);
+
+Console.Read();
---
+++ AdventOfCode/Day4/ScratchCardEngine.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day4
+{
+    internal class ScratchCardEngine
+    {
+        private IEnumerable<string> _lines;
+        private List<ScratchCard> _cards = new List<ScratchCard>();
+
+        public ScratchCardEngine(IEnumerable<string> lines)
+        {
+            _lines = lines;
+        }
+
+        public int ProcessValues()
+        {
+            foreach (var line in _lines)
+            {
+                var gameSplit = line.Split(':');
+                Int32.TryParse(string.Concat(gameSplit.First().Where(char.IsDigit)), out int id);
+                var card = new ScratchCard{ Id = id };
+
+                var scoringNums = gameSplit.Last().Split('|').First().Trim().Split(' ').ToList();
+                    scoringNums.RemoveAll(x => x.Trim().Length == 0);
+                var scratchedNums = gameSplit.Last().Split('|').Last().Trim().Split(' ').ToList();
+                    scratchedNums.RemoveAll(x => x.Trim().Length == 0);
+
+                card.ScoringNums = scoringNums.Select(x => Int32.Parse(x)).ToList();
+                card.ScratchedNums = scratchedNums.Select(x => Int32.Parse(x)).ToList();
+
+                _cards.Add(card);
+            }
+
+            return _cards.Sum(x => x.Score);
+        }
+    }
+
+    internal class ScratchCard
+    {
+        public int Id;
+        public List<int> ScoringNums = new List<int>();
+        public List<int> ScratchedNums = new List<int>();
+
+        public int Score
+        {
+            get
+            {
+                return GetScore();
+            }
+        }
+
+        private int GetScore()
+        {
+            int timesScored = 0;
+            foreach (int scoringNum in ScoringNums)
+            {
+                if (ScratchedNums.Contains(scoringNum)) timesScored++;
+            }
+
+            var res = timesScored > 0 ? 1 : 0;
+            for (int i = 1; i < timesScored; i++)
+            {
+                res = res * 2;
+            }
+            return res;
+        }
+    }
+}
